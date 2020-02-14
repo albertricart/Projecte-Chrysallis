662ec0862561b898e0f12a6d6d8fc467492b5d86
JRiberaG <51130683+JRiberaG@users.noreply.github.com>
2020-02-14 13:28:06 +0100

    añadido ORM

---
+++ App Escritorio/Projecte Chrysallis/Base de Datos/ORM.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecte_Chrysallis.Base_de_Datos
+{
+    public static class ORM
+    {
+        //LINQ
+
+        public static ChrysallisEntities bd = new ChrysallisEntities();
+
+        public static String SaveChanges()
+        {
+            String mensaje = "";
+
+            try
+            {
+                ORM.bd.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                ORM.RejectChanges();
+
+                SqlException sqlEx = (SqlException)ex.InnerException.InnerException;
+                mensaje = ORM.MensajeError(sqlEx);
+            }
+
+            return mensaje;
+        }
+
+        public static void RejectChanges()
+        {
+            foreach (DbEntityEntry entry in bd.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private static string MensajeError(SqlException sqlEx)
+        {
+            string mensaje = "";
+
+            switch (sqlEx.Number)
+            {
+                case -1:
+                    mensaje = "Error de conexión con el servidor";
+                    break;
+                case 547:
+                    mensaje = "Tiene datos relacionados";
+                    break;
+                case 2601:
+                case 2627:
+                    mensaje = "Tiene datos duplicados";
+                    break;
+                case 4060:
+                    mensaje = "No se encuentra la base de datos";
+                    break;
+                case 18456:
+                    mensaje = "Usuario o contraseña incorrectos";
+                    break;
+                default:
+                    mensaje = "(" + sqlEx.Number + ") " + sqlEx.Message;
+                    break;
+            }
+
+            return mensaje;
+        }
+    }
+}
---
+++ App Escritorio/Projecte Chrysallis/Base de Datos/ORM_Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecte_Chrysallis.Base_de_Datos
+{
+    public static class ORM_Admin
+    {
+        //select
+        //insert
+        //update
+        //delete
+        //public static List<Administradores>
+    }
+}
