38904a96a4cd8e1da09c0a548844ca82af457b50
JRiberaG <51130683+JRiberaG@users.noreply.github.com>
2020-02-18 12:55:19 +0100

    Nuevos forms

---
+++ App Escritorio/Projecte Chrysallis/Formularios/FormNuevoAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projecte_Chrysallis.Formularios
+{
+    public partial class FormNuevoAdmin : Form
+    {
+        public FormNuevoAdmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
